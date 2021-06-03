6ec3498d93b2325a1cfa3c9369e0494a998604ee
Anuj Datar <anuj.datar@gmail.com>
2021-06-03 15:34:39 +0530

    Added client logo component

---
+++ components/ClientLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <img :src="require('~/assets/images/clients/logo' + image + '.jpg')" alt="">
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: Number,
+      default: 1
+    }
+  }
+}
+</script>
