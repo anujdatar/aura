1119fffa3e2932d982d45fdca6978e9b923942e5
Anuj Datar <anuj.datar@gmail.com>
2021-06-15 14:21:12 +0530

    Added services page and about page

---
+++ pages/about/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="-mt-13 overflow-hidden flex flex-col min-h-screen">
+    <aura-hero :title="heroTitle" :content="heroContent" :image-src="heroImage" />
+    <div class="container mt-20 mb-8 w-full">
+      <div class="flex md:flex-row flex-col justify-center items-center text-justify">
+        <div class="text-justify">
+          <h2 class="font-semibold text-2xl text-blue-gray-700 mb-2 md:text-left text-center">
+            About Aura
+          </h2>
+          <p class="home-info-text">
+            With more than twenty five years of experience each in CNC laser cutting, bending and metal fabrication, the leadership of the company is with the dynamic duo, the founder-directors, Mr. Unmesh Mahajani, B.E. (Mech) from NIT Durgapur and Mr. Abhay Paranjpe, a chartered accountant.
+          </p>
+          <p class="home-info-text">
+            Both the Directors possess the capability of understanding diverse product requirements and having dealt with a diverse set of clients, they can offer valuable feedbacks and suggestions to customers to improve their products and bottom line.Our vision is to capture a signigicant market share in the busiuness of metal fabrication and assembly.
+          </p>
+          <p class="home-info-text">
+            A team of around 100 + Qualified Employees, highly competent technicians and engineers are the strong pillars of this organization. The employees possess requisite domain experience in CNC Technology, Engineering, Sheet Metal Processing – Laser Cutting, Plasma Cutting, Bending, Rolling, Machining, Precision Fabrication, Assembly and Quality Assurance.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="container mt-8 mb-30 w-full">
+      <div class="flex md:flex-row flex-col justify-center items-center">
+        <div class="text-justify">
+          <h2 class="font-semibold text-2xl text-blue-gray-700 mb-2 md:text-left text-center">
+            Our Mission
+          </h2>
+          <p class="home-info-text">
+            Our mission is to provide a complete solution to customer requirements, nurture and maintain ethical values, focus on quality in ever business provess while fostering Continuous Improvement in our in house systems.
+          </p>
+          <h2 class="font-semibold text-2xl text-blue-gray-700 mb-2 md:text-left text-center pt-8">
+            Our Vision
+          </h2>
+          <p class="home-info-text">
+            Our vision is to capture a signigicant market share in the busiuness of metal fabrication and assembly.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="relative bg-white">
+      <the-slant />
+      <div class="container flex-col pb-18">
+        <h2 class="text-2xl block text-blue-gray-700 font-semibold text-center mb-10">
+          How we can help
+        </h2>
+        <div class="flex flex-row flex-wrap items-center justify-center relative">
+          <about-card :image-src="card1.imageSrc" :card-content="card1.content" />
+          <about-card :image-src="card2.imageSrc" :card-content="card2.content" />
+          <about-card :image-src="card3.imageSrc" :card-content="card3.content" />
+          <about-card :image-src="card4.imageSrc" :card-content="card4.content" />
+          <about-card :image-src="card5.imageSrc" :card-content="card5.content" />
+          <about-card :image-src="card6.imageSrc" :card-content="card6.content" />
+          <about-card :image-src="card7.imageSrc" :card-content="card7.content" />
+        </div>
+      </div>
+    </div>
+    <div class="relative bg-blue-gray-200 mt-auto">
+      <the-slant color="text-blue-gray-200" />
+      <aura-footer />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      heroTitle: 'The Largest Setup of Precision Metal Cutting & Fabrication',
+      heroContent: 'CNC Laser Cutting, CNC Plasma Cutting, CNC Bending, CNC Rolling, CNC Straightening, Welding',
+      heroImage: require('~/assets/images/hero_about.jpeg'),
+      card1: {
+        imageSrc: require('~/assets/images/card_images/sheet.jpeg'),
+        content: 'Versatile resource pool to handle variety and complexity'
+      },
+      card2: {
+        imageSrc: require('~/assets/images/card_images/production.jpg'),
+        content: 'Ability to handle low to high production quantities'
+      },
+      card3: {
+        imageSrc: require('~/assets/images/card_images/team2.jpg'),
+        content: 'Competent and experienced teams'
+      },
+      card4: {
+        imageSrc: require('~/assets/images/card_images/plasma.jpeg'),
+        content: 'Advanced technology with CNC Laser, Plasma, Bending, and Rolling machines'
+      },
+      card5: {
+        imageSrc: require('~/assets/images/card_images/plant2.png'),
+        content: 'Capacity to augment machinary and plant setup'
+      },
+      card6: {
+        imageSrc: require('~/assets/images/card_images/highway.jpg'),
+        content: 'Excellent connectivity through Pune-Bangalore National Highway'
+      },
+      card7: {
+        imageSrc: require('~/assets/images/card_images/port.jpg'),
+        content: 'Proximity to inland container depots & Mumbai Port'
+      }
+    }
+  },
+  head () {
+    return { title: 'About' }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ pages/services/index.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="-mt-13 overflow-hidden flex flex-col min-h-screen">
+    <aura-hero :title="heroTitle" :content="heroContent" :image-src="heroImage" image-offset="75%" />
+    <div class="container mt-8 mb-30">
+      <div class="flex md:flex-row flex-col justify-center items-center">
+        <div class="text-justify">
+          <h2 class="font-semibold text-2xl text-blue-gray-700 mb-2 md:text-left text-center">
+            Infrastructure &amp; Technology
+          </h2>
+          <p class="home-info-text">
+            Aura LaserFab can fabricate components ranging from less than a kg to five tons. Aura LaserFab offers both manufacturing and job shopping services in the field of Metal Fabrication &amp; Assembly with help of CNC Laser Cutting, CNC Bending, CNC Rolling, Machining and various fabrication &amp; assembly processes
+          </p>
+          <p class="home-info-text">
+            Focusing on Quality at every stage of manufacturing we strive to find better solutions to the problems of client. This enables our customers to realize the optimum value of their products and improve their competitiveness and profitability.
+          </p>
+          <p class="home-info-text">
+            What makes this possible is the thoughtfully developed infrastructure that is more than equipped to undertake challenging jobs and adhere to stringent quality standards.
+          </p>
+          <h2 class="font-semibold text-2xl text-blue-gray-700 mb-2 md:text-left text-center pt-8">
+            Materials
+          </h2>
+          <p class="home-info-text">
+            We buy a wide range of raw materials directly from the manufacturers to ensure good quality, reliability and cost competitiveness of supply. We buy around 800 tons of raw material per month. We procure raw material of various grades confirming to European and Indian Standards of -
+            <ul class="list-disc ml-10">
+              <li>Carbon Steel</li>
+              <li>High Tensile Carbon Steel</li>
+              <li>Galvanized Steel</li>
+              <li>Stainless Steel</li>
+              <li>Aluminium</li>
+              <li>Special Steels like HARDOX, 16Mo3</li>
+            </ul>
+          </p>
+          <h2 class="font-semibold text-2xl text-blue-gray-700 mb-2 md:text-left text-center pt-8">
+            Engineering &amp; Development
+          </h2>
+          <p class="home-info-text">
+            A dedicated Product Development Team ensures the development of components in the shortest possible time. With the use of advanced design softwares like AutoCAD and Sigma Nest - we offer quick support during product development.
+          </p>
+          <p class="home-info-text">
+            Aura Laser offers innovative Value Analysis &amp; Value Engineering (VAVE) proposals without affecting quality &amp; durability of product directly helping customers reduce cost of their products.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="relative bg-white">
+      <the-slant />
+      <div class="container flex-col my-20 md:mt-40">
+        <video-card :card-title="service1.cardTitle" :video-link="service1.videoLink" />
+        <video-card :card-title="service2.cardTitle" :video-link="service2.videoLink" text-right />
+        <video-card :card-title="service3.cardTitle" :video-link="service3.videoLink" />
+        <video-card :card-title="service4.cardTitle" :video-link="service4.videoLink" text-right />
+        <video-card :card-title="service5.cardTitle" :video-link="service5.videoLink" />
+        <video-card :card-title="service6.cardTitle" text-right />
+      </div>
+    </div>
+    <div class="relative bg-blue-gray-200 mt-auto">
+      <the-slant color="text-blue-gray-200" />
+      <aura-footer />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      heroTitle: 'Complete solution provider in metal processing &amp; fabrication',
+      heroContent: 'CNC Laser Cutting, CNC Plasma Cutting, CNC Bending, CNC Rolling, CNC Straightening, Welding',
+      heroImage: require('~/assets/images/hero_services.jpeg'),
+      service1: {
+        cardTitle: 'CNC Laser Cutting',
+        videoLink: 'https://www.youtube-nocookie.com/embed/9k2hIgwluP4'
+      },
+      service2: {
+        cardTitle: 'CNC Plasma Cutting',
+        videoLink: 'https://www.youtube-nocookie.com/embed/451xMlS69Ho'
+      },
+      service3: {
+        cardTitle: 'CNC Bending',
+        videoLink: 'https://www.youtube-nocookie.com/embed/jONKl9u27TY'
+      },
+      service4: {
+        cardTitle: 'CNC Rolling(4 Rolls System)',
+        videoLink: 'https://www.youtube-nocookie.com/embed/dbspCBzFkZ0'
+      },
+      service5: {
+        cardTitle: 'Metal Fabrication',
+        videoLink: 'https://www.youtube-nocookie.com/embed/iN-45MhK8eg'
+      },
+      service6: {
+        cardTitle: 'Surface Treatment'
+      }
+    }
+  },
+  head () {
+    return { title: 'Services' }
+  }
+}
+</script>
+
+<style>
+</style>
