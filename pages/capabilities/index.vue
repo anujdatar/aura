554ba971bde2b4700493151cbb2cfc22ac61b945
Anuj Datar <anuj.datar@gmail.com>
2021-06-15 16:43:07 +0530

    Empty capabilities page for test deploy

---
+++ pages/capabilities/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="-mt-13 overflow-hidden flex flex-col min-h-screen">
+    <aura-hero :title="heroTitle" :content="heroContent" :image-src="heroImage" image-offset="75%" />
+    <aura-timeline />
+    <div class="relative bg-blue-gray-200 mt-auto">
+      <the-slant color="text-blue-gray-200" />
+      <aura-footer />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      heroTitle: 'The Largest Setup of Precision Metal Cutting & Fabrication',
+      heroContent: 'CNC Laser Cutting, CNC Plasma Cutting, CNC Bending, CNC Rolling, CNC Straightening, Welding',
+      heroImage: require('~/assets/images/hero_capabilities.jpeg')
+    }
+  },
+  head () {
+    return { title: 'Capabilities' }
+  }
+}
+</script>
+
+<style>
+</style>
