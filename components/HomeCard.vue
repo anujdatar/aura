d47049f48f0971bfdad308edb138a132b8835705
Anuj Datar <anuj.datar@gmail.com>
2021-06-15 14:05:46 +0530

    converted slot element to image element w/ src as prop

--- components/HomeCard.vue
+++ components/HomeCard.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="relative flex flex-col justify-center items-center border shadow rounded-xl md:w-1/4 w-3/4 md:min-h-84 lg:h-auto ml-10 mr-10 mb-12 p-5 bg-white">
+  <div class="relative flex flex-col justify-center items-center border shadow rounded-xl md:w-1/4 w-full md:min-h-84 lg:h-auto ml-10 mr-10 mb-12 p-5 bg-white">
     <div class="flex rounded-full shadow-black shadow-md justify-center items-center text-center mb-8" :class="circleColor">
       <!-- <div class="w-2/3 h-2/3 fill-current text-white"> -->
       <div class="fill-current absolute -top-5 text-white">
-        <slot />
+        <!-- <slot /> -->
+        <img :src="cardImage" alt="homeCardImage" class="w-20 h-20 rounded-lg">
       </div>
     </div>
     <div class="text-center font-semibold text-lg text-blue-gray-700 mt-4">
@@ -29,6 +30,10 @@ export default {
     cardContent: {
       type: String,
       default: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus a sequi iste.'
+    },
+    cardImage: {
+      type: String,
+      default: ''
     }
   }
 }
