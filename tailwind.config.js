fcf22dc15f2ecb68aed6bbc508c4358049f76a52
Anuj Datar <anuj.datar@gmail.com>
2021-10-21 18:50:53 +0530

    Added tailwind css config file. defined website theme, for refactor

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        accent: '#f97316', // orange-500
+        'accent-alt': '#16a34a', // green-600
+        primary: '#334155', // blue-gray-700
+        secondary: '#64748b', // blue-gray-500
+        alt: '#e2e8f0' // blue-gray-200
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
