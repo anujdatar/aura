233f1e1a860e64e9f45ffc30d44470e3bf62986f
Anuj Datar <anuj.datar@gmail.com>
2021-10-23 20:25:14 +0530

    Renamed slant to section separator, added orange accent/line across the top of the component.

---
+++ components/SectionSeparator.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20.1" style="transform: translateZ(0px);">
+    <svg
+      class="absolute bottom-0 overflow-hidden"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="none"
+      version="1.1"
+      viewBox="0 0 2560 95"
+      x="0"
+      y="0"
+    >
+      <polygon class="fill-current" points="2560,0 2560,95 0,95" :class="color" />
+    </svg>
+    <separator-line />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'text-white'
+    }
+  }
+}
+</script>
---
+++ components/SeparatorLine.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bottom-0 top-auto left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20.1" style="transform: translateZ(0px);">
+    <svg
+      class="absolute bottom-0 overflow-hidden"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="none"
+      version="1.1"
+      viewBox="0 0 2560 100"
+      x="0"
+      y="0"
+    >
+      <polygon class="fill-current" points="0,95 2650,0 2650,5 0,100" :class="color" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'text-accent'
+    }
+  }
+}
+</script>
