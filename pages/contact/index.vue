37166b9db98210b9fdd4a17db6609637f0f6f5ce
Anuj Datar <anuj.datar@gmail.com>
2021-10-25 16:56:20 +0530

    Updated contact page

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,85 +1,53 @@
 <template>
-  <div class="page-content bg-blue-gray-700 text-blue-gray-200  min-h-screen flex flex-col overflow-hidden">
-    <div class="container relative flex flex-col centent-center items-center justify-center mt-20 mb-30">
-      <div class="flex flex-col md:flex-row justify-between">
-        <div class="p-2">
-          <div class="">
-            <h2 class="text-green-600 text-2xl">
-              Aura Laserfab Pvt. Ltd.
-            </h2>
-            <p class="text-xs leading-8">
-              Gat No. 1021, Pune-Bangalore Highway
-            </p>
-            <p class="text-xs leading-8">
-              Near Pune, Shirwal
-            </p>
-            <p class="text-xs leading-8">
-              Dist. Satara - 412801
-            </p>
-            <p class="text-xs leading-8">
-              <strong>Pin: </strong>412 801
-            </p>
-            <p class="text-xs leading-8">
-              <strong>Phone: </strong>+91 21699 244545, +91 2169 244546
-            </p>
-            <p class="text-xs leading-8">
-              <strong>E-mail: </strong><a href="mailto:sales@auralaser.net">sales@auralaser.net</a> | <a href="mailto:laser@auralaser.net">laser@auralaser.net</a>
-            </p>
-          </div>
-          <hr class="mt-3 md:mr-3">
-          <div class="mt-3">
-            <h2 class="text-green-600 text-2xl">
-              Contact Persons
-            </h2>
-            <p class="text-xs leading-8">
-              <strong>Abhay Paranjpe (Director): </strong>+91 9822 051621
-            </p>
-            <p class="text-xs leading-8">
-              <strong>Umesh Mahajani (Director): </strong>+91 9822 051619
-            </p>
-            <p class="text-xs leading-8">
-              <strong>Vivek Peshave (Manager - Marketing): </strong>+91 9881 727472
-            </p>
-            <p class="text-xs leading-8">
-              <strong>Alok Dange (Asst. Manager - Marketing): </strong>+91 9881 727467
-            </p>
-          </div>
-          <hr class="md:hidden block mt-3">
-        </div>
-
-        <div class="md:min-w-436px md:max-h-full min-w-600px h-368px md:p-4 md:pl-12 md:mt-4 pt-8">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3791.2178881282207!2d73.9604123!3d18.153874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2f64204c17821%3A0xd0bec5900841f42c!2sAura%20Laserfab%20Private%20Limited!5e0!3m2!1sen!2sin!4v1622216420647!5m2!1sen!2sin"
-            allowfullscreen=""
-            loading="lazy"
-            class="border-none w-full h-full rounded-xl"
-          />
-        </div>
+  <div class="page-content bg-primary min-h-screen flex flex-col overflow-hidden w-full h-full">
+    <!-- map and contact form div -->
+    <div class="relative mt-24 min-h-screen-sm md:min-h-screen-md mb-30">
+      <!-- map -->
+      <div id="map" class="absolute inset-0 bg-alt">
+        <iframe
+          style="filter: grayscale(.2) contrast(1.2) opacity(.75);"
+          marginheight="0"
+          marginwidth="0"
+          title="map"
+          scrolling="no"
+          src="https://maps.google.com/maps/embed/v1/place?key=AIzaSyALhTi2QKCPFX-Ad8mEhitkdFPdSNq2UAo&q=aura%20laserfab%20shirwal&center=18.153874,73.964&zoom=17"
+          width="100%"
+          height="100%"
+          frameborder="0"
+        />
       </div>
-      <div class="p-8 min-w-4/5 md:max-w-830px bg-blue-gray-200 mt-16 mb-20 rounded-xl mx-auto">
-        <form class="flex flex-col" @submit.prevent="submitFormButton">
-          <div class="input-div">
-            <label for="name" class="input-label">Full Name</label>
-            <input id="name" type="text" name="name" placeholder="Full Name" class="input-field text-blue-gray-800">
+      <!-- contact form -->
+      <div class="px-16 py-24 mx-auto my-10 flex">
+        <form class="w-full md:w-1/2 bg-alt rounded-lg p-8 flex flex-col relative z-10 shadow-md md:ml-auto" @submit.prevent="submitFormButton">
+          <h2 class="text-primary text-2xl font-semibold mb-4">
+            Contact Us
+          </h2>
+          <div class="relative mb-4">
+            <label for="name" class="form-label">Name</label>
+            <input id="name" type="text" name="name" class="form-input">
           </div>
-          <div class="input-div">
-            <label for="email" class="input-label">Email</label>
-            <input id="email" type="email" name="email" placeholder="Email" class="input-field text-blue-gray-800">
+          <div class="relative mb-4">
+            <label for="company" class="form-label">Company</label>
+            <input id="company" type="text" name="company" class="form-input">
           </div>
-          <div class="input-div">
-            <label for="message" class="input-label">Message</label>
-            <textarea id="message" name="message" placeholder="Type a message.." class="input-field text-blue-gray-800" rows="4" />
+          <div class="relative mb-4">
+            <label for="email" class="form-label">Email</label>
+            <input id="email" type="email" name="email" class="form-input">
           </div>
-          <div class="text-center mt-6">
-            <button class="bg-blue-gray-800 text-white active:bg-blue-gray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+          <div class="relative mb-4">
+            <label for="message" class="form-label">Message</label>
+            <textarea id="message" name="message" class="form-input h-32 resize-none" />
+          </div>
+          <div class="text-center mb-4">
+            <button class="form-button" type="submit">
               Send Message
             </button>
           </div>
         </form>
       </div>
     </div>
-    <div class="relative bg-blue-gray-200 mt-auto">
-      <the-slant color="text-blue-gray-200" />
+    <div class="relative bg-alt mt-auto">
+      <section-separator color="text-alt" />
       <aura-footer />
     </div>
   </div>
@@ -92,33 +60,35 @@ export default {
   },
   methods: {
     submitFormButton (e) {
+      // e.preventDefault()
       console.log('Form submitted')
-      e.preventDefault()
     }
   }
 }
 </script>
 
 <style>
-.input-div {
-  @apply w-full;
-  @apply mb-3;
+.form-label {
+  @apply leading-7;
+  @apply text-sm uppercase font-semibold;
+  @apply text-secondary;
 }
-.input-label {
-  @apply uppercase;
-  @apply text-blue-gray-600;
-  @apply font-semibold;
-  @apply mb-8;
-}
-.input-field {
+.form-input {
   @apply w-full;
-  @apply shadow;
-  @apply appearance-none;
-  @apply border-0;
-  @apply rounded-lg;
-  @apply py-2;
-  @apply px-3;
-  @apply leading-tight;
-  @apply text-sm;
+  @apply rounded;
+  @apply border border-alt;
+  @apply focus:border-accent-alt;
+  @apply text-primary;
+  @apply outline-none;
+  @apply py-1 px-3;
+  @apply leading-8;
+  @apply transition-colors duration-300 ease-in-out;
+}
+.form-button {
+  @apply bg-primary hover:bg-secondary border active:border-accent-alt text-alt;
+  @apply uppercase text-sm font-bold;
+  @apply px-6 py-3;
+  @apply rounded shadow hover:shadow-lg outline-none focus:outline-none;
+  @apply ease-linear transition-all duration-150;
 }
 </style>
