90350df0b804626903bacadf95a0430850c3e533
Anuj Datar <anuj.datar@gmail.com>
2021-10-25 16:57:30 +0530

    Updated alternate-accent color in tailwind-config theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       colors: {
         accent: '#f97316', // orange-500
-        'accent-alt': '#16a34a', // green-600
+        'accent-alt': '#14b8a6', // teal-700
         primary: '#334155', // blue-gray-700
         secondary: '#64748b', // blue-gray-500
         alt: '#e2e8f0' // blue-gray-200
