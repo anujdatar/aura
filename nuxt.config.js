9717079ed5181a8ee02a9bc1a6b6cdb7a5d8ff42
Anuj Datar <anuj.datar@gmail.com>
2022-02-16 16:58:03 +0530

    Added proper file loader to handle pdf files

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,5 +44,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend (config, ctx) {
+      config.module.rules.push({
+        test: /\.pdf$/,
+        loader: 'file-loader'
+      })
+    }
   }
 }
