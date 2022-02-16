e7679bf9e1feefc9a5d188f64332ff5dd94041cc
Anuj Datar <anuj.datar@gmail.com>
2022-02-16 16:54:59 +0530

    Added iso-cert component to footer. Reduced max width of footer, so it looks like the top navbar

--- components/AuraFooter.vue
+++ components/AuraFooter.vue
@@ -1,27 +1,30 @@
 <template>
   <div
-    class="text-sm flex flex-col justify-center text-center mt-auto pt-10"
+    class="text-sm flex flex-col items-center justify-center text-center mt-auto pt-10"
     :class="textColor"
   >
-    <div class="flex flex-row w-full justify-between">
+    <div class="flex flex-row w-full max-w-1200px justify-between">
       <div class="text-left ml-4">
         <h2 class="text-lg text-primary">
           Aura Laserfab Pvt. Ltd.
         </h2>
         <p class="text-sm leading-6">
-          Gat No. 1021, Pune-Bangalore Highway, Near Pune, Shirwal
+          Gat No. 1021, Pune-Bangalore Highway, Near Pune,
         </p>
         <p class="text-sm leading-6">
-          Dist. Satara - 412801
+          Shirwal, Dist. Satara - 412801
         </p>
       </div>
-      <div class="flex items-end">
-        <a href="https://www.youtube.com/channel/UCM-qRH75zRIPxla2Ty-2MYQ" target="_blank" rel="noopener noreferrer">
-          <youtube-icon class="w-8 text-primary mr-1" />
-        </a>
-        <a href="https://linkedin.com/company/aura-laserfab-private-limited-pune" target="_blank" rel="noopener noreferrer">
-          <linkedin-icon class="w-8 text-primary mr-2" />
-        </a>
+      <div class="flex flex-col justify-between">
+        <iso-cert-display />
+        <div class="flex items-end ml-auto">
+          <a href="https://www.youtube.com/channel/UCM-qRH75zRIPxla2Ty-2MYQ" target="_blank" rel="noopener noreferrer">
+            <youtube-icon class="w-8 text-primary mr-1" />
+          </a>
+          <a href="https://linkedin.com/company/aura-laserfab-private-limited-pune" target="_blank" rel="noopener noreferrer">
+            <linkedin-icon class="w-8 text-primary mr-2" />
+          </a>
+        </div>
       </div>
     </div>
     <div class="pt-3 leading-8">
