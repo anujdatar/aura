05eb0c906c6dad4c779973f61cf20b89c3a88dea
Anuj Datar <anuj.datar@gmail.com>
2022-02-21 12:32:12 +0530

    Combined both ISO certs. Updated iso component to display single pdf. Fixed iso component sizing issues when placed inside other fixed size components.

--- components/IsoCertDisplay.vue
+++ components/IsoCertDisplay.vue
@@ -1,28 +1,16 @@
 <template>
-  <div class="">
-    <div class="flex text-primary hover:text-accent font-bold mb-2">
-      <div class="flex flex-col mr-2">
-        <p class="cursor-pointer hover:underline" @click="openIsoModal">
-          ISO 9001:2015
-        </p>
-        <p class="cursor-pointer hover:underline" @click="open3834Modal">
-          ISO 3834-2
-        </p>
-      </div>
-      <iso-logo class="w-12" />
+  <div class="iso-modal-div">
+    <div class="flex cursor-pointer hover:text-accent font-bold items-center hover:underline" @click="openIsoModal">
+      <p class="mr-2">
+        ISO 9001:2015
+      </p>
+      <iso-logo class="w-8" />
     </div>
-    <!-- iso 9001 modal -->
-    <div rel="closed" class="absolute top-0 left-0 w-full h-full z-1000 m-auto overflow-hidden iso-9001-modal">
-      <div class="relative mx-auto w-full h-full py-4 overflow-y-scroll z-1001">
+    <!-- iso modal -->
+    <div rel="closed" class="absolute top-0 left-0 w-full h-screen z-1000 m-auto overflow-hidden iso-modal">
+      <div class="relative mx-auto w-full h-full py-4 z-1001">
         <span class="close cursor-pointer z-1001" @click="closeIsoModal">&times;</span>
-        <embed src="aura_iso_certificate.jpg" type="application/pdf" class="mx-auto z-1001">
-      </div>
-    </div>
-    <!-- iso 3834 modal -->
-    <div rel="closed" class="absolute top-0 left-0 w-full h-full z-1000 m-auto overflow-hidden iso-3834-modal">
-      <div class="relative mx-auto w-full h-full py-4 overflow-y-scroll z-1001">
-        <span class="close cursor-pointer z-1001" @click="close3834Modal">&times;</span>
-        <embed src="aura_iso_3834.pdf" type="application/pdf" class="mx-auto z-1001 w-3/4 h-full">
+        <embed src="aura_iso_certificate.pdf" type="application/pdf" class="mx-auto z-1001 w-3/4 h-full">
       </div>
     </div>
   </div>
@@ -33,45 +21,26 @@ import IsoLogo from '@/assets/icons/iso-logo.svg?inline'
 export default {
   components: { IsoLogo },
   computed: {
-    iso9001Modal () {
-      return document.querySelector('.iso-9001-modal')
-    },
-    iso3834Modal () {
-      return document.querySelector('.iso-3834-modal')
+    isoModal () {
+      return document.querySelector('.iso-modal')
     }
   },
   methods: {
     openIsoModal () {
-      this.iso9001Modal.style.display = 'block'
-      this.iso9001Modal.setAttribute('rel', 'open')
+      this.isoModal.style.display = 'block'
+      this.isoModal.setAttribute('rel', 'open')
       window.addEventListener('keyup', this.handleEscKey)
     },
     closeIsoModal () {
-      this.iso9001Modal.style.display = 'none'
-      this.iso9001Modal.setAttribute('rel', 'closed')
-      window.removeEventListener('keyup', this.handleEscKey)
-    },
-    open3834Modal () {
-      this.iso3834Modal.style.display = 'block'
-      this.iso3834Modal.setAttribute('rel', 'open')
-      window.addEventListener('keyup', this.handleEscKey)
-    },
-    close3834Modal () {
-      this.iso3834Modal.style.display = 'none'
-      this.iso3834Modal.setAttribute('rel', 'closed')
+      this.isoModal.style.display = 'none'
+      this.isoModal.setAttribute('rel', 'closed')
       window.removeEventListener('keyup', this.handleEscKey)
     },
     handleEscKey (e) {
-      const isIsoOpen = this.iso9001Modal.getAttribute('rel')
+      const isIsoOpen = this.isoModal.getAttribute('rel')
       if (isIsoOpen === 'open') {
         if (e.key === 'Escape') {
           this.closeIsoModal()
-        }
-      }
-      const is3834Open = this.iso3834Modal.getAttribute('rel')
-      if (is3834Open === 'open') {
-        if (e.key === 'Escape') {
-          this.close3834Modal()
         }
       }
     }
@@ -80,26 +49,14 @@ export default {
 </script>
 
 <style scoped>
-.iso-9001-modal::before {
+.iso-modal::before {
   @apply bg-black opacity-95 fixed top-0 left-0 w-full h-full;
   content: "";
 }
-.iso-9001-modal {
+.iso-modal {
   display: none
 }
-.iso-9001-modal .close {
-  @apply z-1002;
-  @apply font-bold text-4xl text-left;
-  @apply absolute top-8 right-8 text-alt;
-}
-.iso-3834-modal::before {
-  @apply bg-black opacity-95 fixed top-0 left-0 w-full h-full;
-  content: "";
-}
-.iso-3834-modal {
-  display: none
-}
-.iso-3834-modal .close {
+.iso-modal .close {
   @apply z-1002;
   @apply font-bold text-4xl text-left;
   @apply absolute top-8 right-8 text-alt;
