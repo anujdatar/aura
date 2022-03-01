5d4b6ce14b0f9a4de61ac24f2a37c6763cb56c06
Anuj Datar <anuj.datar@gmail.com>
2022-03-01 17:01:09 +0530

    base vitesse repo with name and favicon changed

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Aura LaserFab',
+  meta: [
+    { name: 'description', content: 'Aura Laserfab Pvt. Ltd.' },
+  ],
+})
+</script>
+
+<template>
+  <router-view />
+</template>
