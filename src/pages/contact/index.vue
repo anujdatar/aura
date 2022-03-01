4f0566719d72cbcdc7ba4644bfbe12d4ea82c42b
Anuj Datar <anuj.datar@gmail.com>
2022-03-01 21:11:59 +0530

    added empty about, contact, industries and services pages

---
+++ src/pages/contact/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>contact</h1>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>
---
+++ src/pages/industries/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Industries</h1>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>
