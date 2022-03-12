fffce484b66c76fa70b9f0e6b3027bc6fbe96531
Anuj Datar <anuj.datar@gmail.com>
2022-03-12 16:30:25 +0530

    updated services text, added material specifications

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -8,22 +8,22 @@
             Infrastructure &amp; Technology
           </h2>
           <p class="paragraph">
-            Aura LaserFab can fabricate components ranging from less than a kg
-            to five tons. Aura LaserFab offers both manufacturing and job
+            Aura Laserfab can fabricate components ranging from less than a kg
+            to five tons. Aura Laserfab offers both manufacturing and job
             shopping services in the field of Metal Fabrication &amp; Assembly
-            with help of CNC Laser Cutting, CNC Bending, CNC Rolling, Machining
-            and various fabrication &amp; assembly processes
+            with help of Laser Cutting, Bending, Rolling, Machining and various
+            fabrication &amp; assembly processes.
           </p>
           <p class="paragraph">
             Focusing on Quality at every stage of manufacturing we strive to
             find better solutions to the problems of client. This enables our
-            customers to realize the optimum value of their products and improve
-            their competitiveness and profitability.
+            customers to realize the optimum value of their products and
+            improve their competitiveness and profitability.
           </p>
           <p class="paragraph">
-            What makes this possible is the thoughtfully developed infrastructure
-            that is more than equipped to undertake challenging jobs and adhere
-            to stringent quality standards.
+            What makes this possible is the thoughtfully developed
+            infrastructure that is more than equipped to undertake challenging
+            jobs and adhere to stringent quality standards.
           </p>
           <h2 class="heading text-center md:text-left pt-8">
             Materials
@@ -35,10 +35,13 @@
             raw material of various grades confirming to European and Indian
             Standards of -
             <ul class="list-disc ml-10">
-              <li>Carbon Steel</li>
-              <li>High Tensile Carbon Steel</li>
-              <li>Galvanized Steel</li>
-              <li>Stainless Steel</li>
+              <li>Carbon Steel - IS2062 E250, 1079D, 513D etc.</li>
+              <li>
+                High Tensile Carbon Steel - E350, S355J2, S355MC, S550MC,
+                S690QL, S700MC, QSTE380 etc.
+              </li>
+              <li>Galvanized Steel - GSM 120, GSM 275 etc.</li>
+              <li>Stainless Steel - SS304, SS316, SS410, SS202 etc.</li>
               <li>Aluminium</li>
               <li>Special Steels like HARDOX, 16Mo3</li>
             </ul>
@@ -124,7 +127,7 @@
               No. of Machines: 45
             </p>
             <p class="text-sm">
-              Make: Essab
+              Make: Esab
             </p>
           </div>
         </video-card>
@@ -156,19 +159,19 @@ export default {
       heroContent: 'MIG/MAG/TIG Welding, CNC Machining, Painting, Powder Coating and Assembly',
       heroImage: require('~/assets/images/hero_services.webp'),
       service1: {
-        cardTitle: 'CNC Laser Cutting',
+        cardTitle: 'Laser Cutting',
         videoLink: 'https://www.youtube-nocookie.com/embed/-3EX9fV5uHg'
       },
       service2: {
-        cardTitle: 'CNC Bending',
+        cardTitle: 'Press Brake',
         videoLink: 'https://www.youtube-nocookie.com/embed/90prqWQgdBM'
       },
       service3: {
-        cardTitle: 'CNC Rolling',
+        cardTitle: 'Plate Rolling',
         videoLink: 'https://www.youtube-nocookie.com/embed/8U-TV4Y0rPA'
       },
       service4: {
-        cardTitle: 'Robotic Fabrication',
+        cardTitle: 'Robot Fabrication',
         videoLink: 'https://www.youtube-nocookie.com/embed/HYMsvFHjY6M'
       },
       service5: {
