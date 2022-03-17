97e8c3327d40f705dd3b708709b71649d48b3b3a
Anuj Datar <anuj.datar@gmail.com>
2022-03-17 17:17:28 +0530

    Updated client logos component, using v-for inside component to show all
    instead of old where component was for one logo and v-for was on page

---
+++ components/ClientLogos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-row flex-wrap items-center justify-center mt-4">
+    <!-- <img :src="require('~/assets/images/clients/logo' + image + '.jpg')" alt=""> -->
+    <img
+      v-for="(image, i) in images"
+      :key="i"
+      :src="image"
+      alt="image"
+      class="p-2 h-18"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    images: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
