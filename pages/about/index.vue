8e873d90715994197871855d9bb9d0fbe6055467
Anuj Datar <anuj.datar@gmail.com>
2022-03-17 17:18:40 +0530

    Added the basic styles from home to about, contact and services pages
    Now, even if refreshed, styling does not get messed up

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -179,4 +179,42 @@ export default {
 </script>
 
 <style>
+.page-content {
+  @apply -mt-24 !important;
+}
+.container {
+  @apply max-w-screen-lg;
+  @apply flex;
+  @apply mx-auto;
+  @apply w-3/4;
+  @apply md:w-auto;
+  @apply px-8;
+  @apply mt-30;
+  @apply mb-30;
+}
+.container-inner {
+  @apply flex;
+  @apply flex-col;
+  @apply md:flex-row;
+  @apply justify-center;
+  @apply items-center;
+  @apply text-justify;
+}
+.heading {
+  @apply font-semibold;
+  @apply text-3xl;
+  @apply text-primary;
+  @apply mb-2;
+  /* @apply text-center; */
+}
+.sub-heading {
+  @apply font-semibold;
+  @apply text-xl;
+  @apply text-primary;
+}
+.paragraph {
+  @apply text-secondary;
+  @apply mb-2;
+  @apply text-justify;
+}
 </style>
