d9c884268bcda3b82f8bb821e4a8be280afc38f8
Anuj Datar <anuj.datar@gmail.com>
2022-04-05 16:13:56 +0530

    Added default color scheme to unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -10,6 +10,15 @@ import {
 } from 'unocss'
 
 export default defineConfig({
+  theme: {
+    colors: {
+      'primary': '#334155', // blue-gray-700
+      'secondary': '#64748b', // blue-gray-500
+      'alt': '#e2e8f0', // blue-gray-200
+      'accent': '#f97316', // orange-500
+      'accent-alt': '#14b8a6', // teal-700
+    },
+  },
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
