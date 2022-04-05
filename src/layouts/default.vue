375591953aa26252110936e813e52c74534dd546
Anuj Datar <anuj.datar@gmail.com>
2022-04-05 16:24:54 +0530

    Added details, routes and components to default and home layouts

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+const routes = [
+  { title: 'Home', path: '/' },
+  { title: 'Industries', path: '/industries' },
+  { title: 'Services', path: '/services' },
+  { title: 'About', path: '/about' },
+  { title: 'Contact', path: '/contact' },
+]
+</script>
+
 <template>
   <main class="">
+    <aura-navigation :routes="routes" />
+    <aura-sidenav :routes="routes" />
     <router-view />
-    <aura-footer />
+    <aura-footer class="bg-alt" />
   </main>
 </template>
--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,6 +1,25 @@
+<script setup lang="ts">
+const routes = [
+  { title: 'Home', path: '/' },
+  { title: 'Industries', path: '/industries' },
+  { title: 'Services', path: '/services' },
+  { title: 'About', path: '/about' },
+  { title: 'Contact', path: '/contact' },
+]
+
+onMounted(() => {
+  const navbar = document.querySelector('.navbar')
+  navbar?.classList.remove('text-white')
+  navbar?.classList.add('text-primary')
+})
+</script>
+
 <template>
-  <main class="">
+  <main class="h-full w-full">
+    <aura-navigation :routes="routes" />
+    <aura-sidenav :routes="routes" />
     <router-view />
+    <section-separator />
     <aura-footer />
   </main>
 </template>
