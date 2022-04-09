ed5a779f3321d921a6fe9a03db3b2f62a83847c1
Anuj Datar <anuj.datar@gmail.com>
2022-04-09 18:27:51 +0530

    added new layout that caters for pages with a white bottom footer

---
+++ src/layouts/whiteFooter.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const routes = [
+  { title: 'Home', path: '/' },
+  { title: 'Industries', path: '/industries' },
+  { title: 'Services', path: '/services' },
+  { title: 'About', path: '/about' },
+  { title: 'Contact', path: '/contact' },
+]
+</script>
+
+<template>
+  <main class="h-full w-full">
+    <aura-navigation :routes="routes" />
+    <aura-sidenav :routes="routes" />
+    <router-view />
+    <section-separator />
+    <aura-footer />
+  </main>
+</template>
