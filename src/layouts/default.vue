da8fd8b2c20eba3ef42f872ba088efc83009a825
Anuj Datar <anuj.datar@gmail.com>
2022-04-09 18:25:40 +0530

    added footer-section-separator to the default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -9,10 +9,11 @@ const routes = [
 </script>
 
 <template>
-  <main class="">
+  <main class="h-full w-full">
     <aura-navigation :routes="routes" />
     <aura-sidenav :routes="routes" />
     <router-view />
+    <section-separator color="text-alt" />
     <aura-footer class="bg-alt" />
   </main>
 </template>
