303da9477b046cb77d3ee0fcef3732d4401a4b9d
Anuj Datar <anuj.datar@gmail.com>
2022-04-11 16:35:22 +0530

    updated icon size from em to rem

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -16,7 +16,7 @@ const routes = [
     <div class="page-content">
       <div class="container flex-col items-center align-middle justify-center">
         <div class="flex flex-col items-center align-middle justify-center text-4xl pt-8">
-          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
+          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="2rem" height="2rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
             <path fill="currentColor" d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm0 26a12 12 0 1 1 12-12a12 12 0 0 1-12 12Z" />
             <path fill="currentColor" d="M15 8h2v11h-2zm1 14a1.5 1.5 0 1 0 1.5 1.5A1.5 1.5 0 0 0 16 22z" />
           </svg>
