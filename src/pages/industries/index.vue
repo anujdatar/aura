e74358ac3d95e9e599cb112b5b49f820d972c83a
Anuj Datar <anuj.datar@gmail.com>
2022-04-14 16:25:46 +0530

    Added section and div wrappers around sections too display as intended

--- src/pages/industries/index.vue
+++ src/pages/industries/index.vue
@@ -23,53 +23,144 @@ const heroContent = 'Earthmoving and construction, Electrical Systems, Mining an
   <aura-hero :image="heroImage" :title="heroTitle" :content="heroContent" />
   <div class="page-content bg-alt">
     <div class="container flex-col">
-      <h2 class="heading">
-        Earthmoving &amp; Construction Equipment
-      </h2>
-      <industry-thumbnails name="earthmoving" :images="earthmoving" />
-      <h2 class="heading">
-        Electrical Switchgear Products
-      </h2>
-      <industry-thumbnails name="switchgear" :images="switchgear" />
-      <h2 class="heading">
-        Mining Equipment
-      </h2>
-      <industry-thumbnails name="mining" :images="mining" />
-      <h2 class="heading">
-        Agriculture and Farm Equipment
-      </h2>
-      <industry-thumbnails name="agriculture" :images="agriculture" />
-      <h2 class="heading">
-        Material Handling Equipment
-      </h2>
-      <industry-thumbnails name="handling" :images="material_handling" />
-      <h2 class="heading">
-        Automotive Components
-      </h2>
-      <industry-thumbnails name="automotive" :images="automotive" />
-      <h2 class="heading">
-        Textile Machinery
-      </h2>
-      <industry-thumbnails name="textile" :images="textile" />
-      <h2 class="heading">
-        Packaging &amp; Printing Machinery
-      </h2>
-      <industry-thumbnails name="packaging" :images="packaging" />
-      <h2 class="heading">
-        Automated Parking Systems
-      </h2>
-      <industry-thumbnails name="parking" :images="parking" />
-      <h2 class="heading">
-        Pollution Control Equipment
-      </h2>
-      <industry-thumbnails name="pollution" :images="pollution" />
-      <h2 class="heading">
-        Process Plant &amp; Equipment
-      </h2>
-      <industry-thumbnails name="process" :images="process" />
+      <!-- 1. earthmoving and construction section -->
+      <section id="earthmoving" class="industry">
+        <h2 class="heading">
+          Earthmoving &amp; Construction Equipment
+        </h2>
+        <p>
+          We supply fabricated and machined assemblies such as Dozers, Linkages, Platforms, Sliders and Carriers for Excavators, Chassis &amp; Operator Platforms for Compactors and Other Cut, Bend and Fabricated Components Loaders, Skid Steers, Engine mounts etc.
+        </p>
+        <industry-thumbnails name="earthmoving" :images="earthmoving" />
+      </section>
+      <!-- 2. electrical switchgear section -->
+      <section id="switchgear" class="industry">
+        <h2 class="heading">
+          Electrical Switchgear Products
+        </h2>
+        <p>
+          Typical Components we supply include SF6 Stainless Steel Welded Tanks for Medium Voltage Switchgear, Breaker Mechanism Units, Enclosures, and Other Precision Mechanism Components.
+        </p>
+        <industry-thumbnails name="switchgear" :images="switchgear" />
+      </section>
+      <!-- 3. mining equipment section -->
+      <section id="mining" class="industry">
+        <h2 class="heading">
+          Mining Equipment
+        </h2>
+        <p>
+          We supply components such as Conveyor Frames, Hydraulic &amp; Fuel Tanks, Engine Base, Lift Axel Assembly etc.
+        </p>
+        <industry-thumbnails name="mining" :images="mining" />
+      </section>
+    </div>
+    <section-separator />
+    <div class="bg-white">
+      <div class="container flex-col">
+        <!-- 4. agriculture and farm equipment section -->
+        <section id="agriculture" class="industry">
+          <h2 class="heading">
+            Agriculture and Farm Equipment
+          </h2>
+          <p>
+            Our Products include Painted Tractor Chassis &amp; Body Parts, Hydraulic and Fuel Tanks, Pedals, Stainless Steel Tanks  &amp; Frames
+          </p>
+          <industry-thumbnails name="agriculture" :images="agriculture" />
+        </section>
+        <!-- 5. material handling section -->
+        <section id="mat_handling" class="industry">
+          <h2 class="heading">
+            Material Handling Equipment
+          </h2>
+          <p>
+            Typical Components include Apron Cells &amp; Buckets for Conveyors, Frames of Baggage Handling Conveyors, Buckets &amp; Chassis of Mini Dumpers and other assemblies
+          </p>
+          <industry-thumbnails name="handling" :images="material_handling" />
+        </section>
+        <!-- 6. other industries section -->
+        <section id="other" class="industry">
+          <h2 class="heading">
+            Other Industries
+          </h2>
+          <div class="flex flex-row flex-wrap sub-industries">
+            <!-- 6.a. automotive components div -->
+            <div id="automotive" class="sub-industry">
+              <h4 class="text-xl">
+                Automotive Components
+              </h4>
+              <p>
+                Products include Trailer Chassis, Landing Gear for Trailers, Parts for Suspension Systems
+              </p>
+              <industry-thumbnails name="automotive" :images="automotive" />
+            </div>
+            <!-- 6.b. textile machinary div -->
+            <div id="textile" class="sub-industry">
+              <h4 class="text-xl">
+                Textile Machinery
+              </h4>
+              <p>
+                We supply Precision Cut &amp; Bent Components like Ring Frames, Suction Ducts, etc
+              </p>
+              <industry-thumbnails name="textile" :images="textile" />
+            </div>
+            <!-- 6.c. packaging and printing machinary div -->
+            <div id="packaging" class="sub-industry">
+              <h4 class="text-xl">
+                Packaging &amp; Printing Machinery
+              </h4>
+              <p>
+                Typical products we supply include Fabricated &amp; Powder Coated Frames, Laser Cut &amp; Bent Parts
+              </p>
+              <industry-thumbnails name="packaging" :images="packaging" />
+            </div>
+            <!-- 6.d. parking systems div -->
+            <div id="parking" class="sub-industry">
+              <h4 class="text-xl">
+                Automated Parking Systems
+              </h4>
+              <p>
+                Typical products we supply include Welded Platforms, Pillars &amp; slides.
+              </p>
+              <industry-thumbnails name="parking" :images="parking" />
+            </div>
+            <!-- 6.e. pollution control div -->
+            <div id="pollution" class="sub-industry">
+              <h4 class="text-xl">
+                Pollution Control Equipment
+              </h4>
+              <p>
+                We supply Laser Cut Bag Holding Plates Fabricated Bag Houses &amp; other Assemblies
+              </p>
+              <industry-thumbnails name="pollution" :images="pollution" />
+            </div>
+            <!-- 6.f. process plant equipment div -->
+            <div id="process" class="sub-industry">
+              <h4 class="text-xl">
+                Process Plant &amp; Equipment
+              </h4>
+              <p>
+                Typical products we supply include - Fabricated &amp; Painted Assemblies Precision Cut &amp; Bent Parts
+              </p>
+              <industry-thumbnails name="process" :images="process" />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   </div>
 </template>
+
+<style>
+.industry {
+  margin-bottom: 3rem;
+}
+.sub-industries > div {
+  width: 100%;
+  max-width: 50%;
+  margin-bottom: 2rem;
+  padding: 1rem;
+}
+</style>
 
 <route lang="yaml">
 meta:
