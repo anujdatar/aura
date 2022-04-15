bd0db6f4aeb663df0f54edab2727bce4f4b44d85
Anuj Datar <anuj.datar@gmail.com>
2022-04-15 16:15:00 +0530

    Added exports route to all the layouts

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -4,6 +4,7 @@ const routes = [
   { title: 'Home', path: '/' },
   { title: 'Industries', path: '/industries' },
   { title: 'Services', path: '/services' },
+  { title: 'Exports', path: '/exports' },
   { title: 'About', path: '/about' },
   { title: 'Contact', path: '/contact' },
 ]
