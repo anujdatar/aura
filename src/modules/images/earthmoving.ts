60859da85b91f03eaa1aae0583c91981703a1dc0
Anuj Datar <anuj.datar@gmail.com>
2022-04-16 21:36:37 +0530

    Fix: using the same ~/../../ for all industry lightbox images

--- src/modules/images/earthmoving.ts
+++ src/modules/images/earthmoving.ts
@@ -1,50 +1,50 @@
 export const earthmoving = [
   {
-    src: 'assets/images/industries/earthmoving/1_Dozer.webp',
+    src: '~/../../assets/images/industries/earthmoving/1_Dozer.webp',
     title: 'Dozer',
   },
   {
-    src: 'assets/images/industries/earthmoving/2_Excavator_Platform.jpg',
+    src: '~/../../assets/images/industries/earthmoving/2_Excavator_Platform.jpg',
     title: 'Excavator Platform',
   },
   {
-    src: 'assets/images/industries/earthmoving/3_Carrier.jpg',
+    src: '~/../../assets/images/industries/earthmoving/3_Carrier.jpg',
     title: 'Carrier',
   },
   {
-    src: 'assets/images/industries/earthmoving/4_Slider.jpg',
+    src: '~/../../assets/images/industries/earthmoving/4_Slider.jpg',
     title: 'Slider',
   },
   {
-    src: 'assets/images/industries/earthmoving/5_Compactor_Chassis.webp',
+    src: '~/../../assets/images/industries/earthmoving/5_Compactor_Chassis.webp',
     title: 'Compactor Chassis',
   },
   {
-    src: 'assets/images/industries/earthmoving/6_Compactor_Chassis.webp',
+    src: '~/../../assets/images/industries/earthmoving/6_Compactor_Chassis.webp',
     title: 'Compactor Chassis',
   },
   {
-    src: 'assets/images/industries/earthmoving/7_Compactor_Operator_Platform.webp',
+    src: '~/../../assets/images/industries/earthmoving/7_Compactor_Operator_Platform.webp',
     title: 'Compactor Operator Platform',
   },
   {
-    src: 'assets/images/industries/earthmoving/8_Back_Hoe_Loader_Weld_Assy_QSTE_380-20mm_thick.webp',
+    src: '~/../../assets/images/industries/earthmoving/8_Back_Hoe_Loader_Weld_Assy_QSTE_380-20mm_thick.webp',
     title: 'Back Hoe Loader Weld Assy QSTE380 &#10142; 20mm thick',
   },
   {
-    src: 'assets/images/industries/earthmoving/9_BUCKETS-ROLLED-8mmX800mm.webp',
+    src: '~/../../assets/images/industries/earthmoving/9_BUCKETS-ROLLED-8mmX800mm.webp',
     title: 'Rolled Buckets &#10142; 8mm x 800mm',
   },
   {
-    src: 'assets/images/industries/earthmoving/10_CUT_BENT_PARTS_6,8,10,12,20mm_S355.webp',
+    src: '~/../../assets/images/industries/earthmoving/10_CUT_BENT_PARTS_6,8,10,12,20mm_S355.webp',
     title: 'Cut & Bent Parts &#10142; 6, 8, 10, 12, 20mm S355',
   },
   {
-    src: 'assets/images/industries/earthmoving/11_CUT_BENT_PARTS_6,8,10,12,20mm_S355.webp',
+    src: '~/../../assets/images/industries/earthmoving/11_CUT_BENT_PARTS_6,8,10,12,20mm_S355.webp',
     title: 'Cut & Bent Parts &#10142; 6, 8, 10, 12, 20mm S355',
   },
   {
-    src: 'assets/images/industries/earthmoving/13_Welded_Loader_Arm-S335-6,8,12mm_thick.webp',
+    src: '~/../../assets/images/industries/earthmoving/13_Welded_Loader_Arm-S335-6,8,12mm_thick.webp',
     title: 'Welded Loader Arm S335 &#10142; 6, 8, 12mm thick',
   },
 ]
