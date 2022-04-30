58cf404e64ae54fef3bd921b447d76f6dbee11fb
Anuj Datar <anuj.datar@gmail.com>
2022-04-30 17:23:13 +0530

    Added new escalator-elevator images for the industries page

---
+++ src/modules/images/escalators.ts
@@ -0,0 +1,30 @@
+export const escalators = [
+  {
+    src: '~/../../assets/images/industries/escalators/1_side_frame_assembly.webp',
+    title: 'Side Frame Assembly',
+  },
+  {
+    src: '~/../../assets/images/industries/escalators/2_welded_assembly.webp',
+    title: 'Welded Assembly',
+  },
+  {
+    src: '~/../../assets/images/industries/escalators/3_wheel_assembly.webp',
+    title: 'Wheel Assembly',
+  },
+  {
+    src: '~/../../assets/images/industries/escalators/4_ribs.webp',
+    title: 'Ribs',
+  },
+  {
+    src: '~/../../assets/images/industries/escalators/5_ribs.webp',
+    title: 'Ribs',
+  },
+  {
+    src: '~/../../assets/images/industries/escalators/6_brackets.webp',
+    title: 'Brackets',
+  },
+  {
+    src: '~/../../assets/images/industries/escalators/7_brackets.webp',
+    title: 'Brackets',
+  },
+]
