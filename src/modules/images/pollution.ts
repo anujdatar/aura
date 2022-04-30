edfaa8815a810964c221a038c14ed9677f48ed9b
Anuj Datar <anuj.datar@gmail.com>
2022-04-30 15:17:35 +0530

    Updated the correct process industries image source file.

--- src/modules/images/pollution.ts
+++ src/modules/images/pollution.ts
@@ -23,36 +23,4 @@ export const pollution = [
     src: '~/../../assets/images/industries/pollution/6_tube_sheet_assy.jpg',
     title: 'Tube Sheet Assembly',
   },
-  {
-    src: '~/../../assets/images/industries/process/1_aeration_element.jpg',
-    title: 'Aeration Element',
-  },
-  {
-    src: '~/../../assets/images/industries/process/2_aeration_element.jpg',
-    title: 'Aeration Element',
-  },
-  {
-    src: '~/../../assets/images/industries/process/3_aeration_element.jpg',
-    title: 'Aeration Element',
-  },
-  {
-    src: '~/../../assets/images/industries/process/4_aeration_element.jpg',
-    title: 'Aeration Element',
-  },
-  {
-    src: '~/../../assets/images/industries/process/5_air_ventilation_unit.jpg',
-    title: 'Air Ventilation Unit',
-  },
-  {
-    src: '~/../../assets/images/industries/process/6_air_ventilation_unit.jpg',
-    title: 'Air Ventilation Unit',
-  },
-  {
-    src: '~/../../assets/images/industries/process/7_cement_plant_equipment.jpg',
-    title: 'Cement Plant Equipment',
-  },
-  {
-    src: '~/../../assets/images/industries/process/8_cement_plant_equipment.jpg',
-    title: 'Cement Plant Equipment',
-  },
 ]
--- src/modules/images/process.ts
+++ src/modules/images/process.ts
@@ -31,4 +31,28 @@ export const process = [
     src: '~/../../assets/images/industries/process/8_cement_plant_equipment.jpg',
     title: 'Cement Plant Equipment',
   },
+  {
+    src: '~/../../assets/images/industries/pollution/1_tube_sheets.jpg',
+    title: 'Tube Sheets',
+  },
+  {
+    src: '~/../../assets/images/industries/pollution/2_tube_sheets.jpg',
+    title: 'Tube Sheets',
+  },
+  {
+    src: '~/../../assets/images/industries/pollution/3_tube_sheets.jpg',
+    title: 'Tube Sheets',
+  },
+  {
+    src: '~/../../assets/images/industries/pollution/4_tube_sheets.jpg',
+    title: 'Tube Sheets',
+  },
+  {
+    src: '~/../../assets/images/industries/pollution/5_tube_sheet_assy.jpg',
+    title: 'Tube Sheet Assembly',
+  },
+  {
+    src: '~/../../assets/images/industries/pollution/6_tube_sheet_assy.jpg',
+    title: 'Tube Sheet Assembly',
+  },
 ]
